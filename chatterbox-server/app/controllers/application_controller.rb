f65208a7f0e9dd74da5c3d485f1d7bce163ca608
Sean Martin <sean@seanmartinstudio.com>
2022-06-25 13:53:02 -0700

    commit tree 17ec34e4177e8a9dcd665c10bfe2b509d75a6fea

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
-  
+  get '/messages' do
+    messages = Message.all_messages 
+    messages.to_json
+  end
+
+  post '/messages' do 
+    messages =  Message.create (
+      body: params[:body]
+      username: params[:username]
+    )
+  end
+
 end
