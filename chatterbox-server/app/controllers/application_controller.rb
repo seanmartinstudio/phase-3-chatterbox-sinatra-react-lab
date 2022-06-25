2d73d9d6078837cbdf658c3e333095039309fcfd
Sean Martin <sean@seanmartinstudio.com>
2022-06-25 14:16:06 -0700

    Complete

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,32 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  get '/messages' do
+    messages = Message.all_messages 
+    messages.to_json
+  end
+
+  post '/messages' do 
+    messages =  Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    messages.to_json
+  end
+
+  patch '/messages/:id' do
+    messages = Message.find(params[:id])
+    messages.update(
+      body: params[:body]
+    )
+    messages.to_json
+  end
+
+  delete '/messages/:id' do
+    messages = Message.find(params[:id])
+    messages.delete
+    messages.to_json
+  end
   
+
 end
--- chatterbox-server/app/models/message.rb
+++ chatterbox-server/app/models/message.rb
@@ -1,3 +1,9 @@
 class Message < ActiveRecord::Base
     
+    
+    def self.all_messages
+        self.all.order(:body)
+    end
+
+
 end
